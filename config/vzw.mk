3583185e0343e757d45a4f02829716fd9e5029c6
somber73 <nytesblood@gmail.com>
2014-03-19 20:31:57 -0400

    lets get data

---
+++ config/vzw.mk
@@ -0,0 +1,9 @@
+#Permissions and Lib for Verizon Wireless Apps
+PRODUCT_PACKAGES += \
+    VerizonSSO
+
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/du/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/du/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/du/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
